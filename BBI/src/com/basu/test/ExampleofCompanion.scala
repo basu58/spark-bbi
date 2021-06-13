ac413ee94ac80754646951bc8b935faf7681d667
basu58 <basudevchhotray@gmail.com>
2021-06-13 10:57:53 -0700

    companion object

---
+++ BBI/src/com/basu/test/ExampleofCompanion.scala
@@ -0,0 +1,29 @@
+package com.basu.test
+// A Scala program to illustrate
+// the concept of the Companion object
+
+// Companion class
+class ExampleofCompanion
+{
+	
+	// Variables of Companion class
+	var str1 = "GeeksforGeeks";
+	var str2 = "Tutorial of Companion object";
+	
+	// Method of Companion class
+	def show()
+	{
+		println(str1);
+		println(str2);
+	}
+}
+
+// Companion object
+object ExampleofCompanion
+{
+	def main(args: Array[String])
+	{
+		var obj = new ExampleofCompanion();
+		obj.show();
+	}
+}
